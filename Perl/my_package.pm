44c8171580ee7708e1e0565038b1e321cbe996e8
Hukanzen <qazxswed1934@gmail.com>
2017-10-03 02:22:20 +0900

    config ファイル関係

--- Perl/my_package.pm
+++ Perl/my_package.pm
@@ -1,5 +1,8 @@
 package my_package.pm
 
+# 第一引数には，パッケージ名($pack_name)が入る
+# メゾッド -> メゾッドの場合，明示的に渡す
+
 sub new
 {
 
@@ -58,4 +61,52 @@ sub put_file
 	open(WF,">>",$file);
 	print WF $data;
 	close(WF);
+}
+
+
+# config ファイル
+my $conf_file;
+
+# config ファイルを決定する
+sub set_conf
+{
+	my ($pack_name,$file)=@_;
+
+	$conf_file=$file;
+}
+
+# コンフィグファイルから指定の値を取得する.
+sub get_conf
+{
+	my ($pack_name,$str)=@_;
+
+	# EXPR の中のすべての ASCII 非英数字キャラクタをバックスラッシュで エスケープしたものを返します。 
+	# (つまり、/[A-Za-z_0-9]/ にマッチしない全ての ASCII 文字の前には ロケールに関わらずバックスラッシュが前置されます。)
+	# これは、ダブルクォート文字列での \Q エスケープを実装するための 内部関数です。 
+	$str=quotemeta(${str});
+
+	my $cnf=$conf_file;
+	
+	my @data=array_load_file($pack_name,$cnf);
+	my $BEGIN=0;
+
+	foreach(@data){
+		if($_ eq "BEGIN"){
+			# BEGINから以下を読み込む
+			$BEGIN=1;
+			next;
+		}
+
+		if($BEGIN==0 or $_=~ m/^#/){
+			next;
+		}elsif($_ eq "END"){
+			# ENDから以下は読みこまない
+			last;
+		}elsif($_=~m/${str}/){
+			# 指定された項目の値を取得し、返す
+			$_=~s/^${str}\s*=\s*//i;
+			$_=~s/\n//;
+			return $_;
+		}
+	}
 }
